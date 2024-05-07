307b8974e3e6c242c220a9b0e5bf6d24de53fff3
rafaelRueda <rrrm85@gmail.com>
2024-05-07 14:53:59 -0500

    add test for users and posts

---
+++ test/test_posts.py
@@ -0,0 +1,40 @@
+from typing import List
+from app import schemas
+
+
+def test_get_all_posts(authorized_client, test_posts):
+    res = authorized_client.get("/posts/")
+
+    # convert the dict in a schema
+    def validate(post):
+        return schemas.PostOut(**post)
+
+    posts_map = map(validate, res.json())
+    posts_list = list(posts_map)
+
+    assert len(res.json()) == len(test_posts)
+    assert res.status_code == 200
+    # assert posts_list[0].Post.id == test_posts[0].id not work becouse the result is not order
+
+
+def test_unathorized_user_get_all_posts(client, test_posts):
+    res = client.get("/posts/")
+    assert res.status_code == 401
+
+
+def test_unathorized_user_get_one_post(client, test_posts):
+    res = client.get(f"/posts/{test_posts[0].id}")
+    assert res.status_code == 401
+
+
+def test_get_one_post_not_exist(authorized_client, test_posts):
+    res = authorized_client.get(f"/posts/888988")
+    assert res.status_code == 404
+
+
+def test_get_one_post(authorized_client, test_posts):
+    res = authorized_client.get(f"/posts/{test_posts[0].id}")
+    post = schemas.PostOut(**res.json())
+    assert post.Post.id == test_posts[0].id
+    assert post.Post.content == test_posts[0].content
+    assert post.Post.title == test_posts[0].title
--- test/test_users.py
+++ test/test_users.py
@@ -5,27 +5,16 @@ from jose import jwt
 from app.config import settings
 
 
-@pytest.fixture
-def test_user(client):
-    user_data = {"email": "hola@gmail.com", "password": "pas123"}
-    res = client.post("/users/", json=user_data)
-
-    assert res.status_code == 201
-    new_user = res.json()
-    new_user["password"] = user_data["password"]
-    return new_user
-
-
-def test_root(client):
-    res = client.get("/")
-    print(res.json().get("messsage"))
-    assert res.json().get("message") == "Welcome to my API!!"
+# def test_root(client):
+#     res = client.get("/")
+#     print(res.json().get("messsage"))
+#     assert res.json().get("message") == "Welcome to my API!!"
 
 
 def test_create_user(client):
     res = client.post("/users/", json={"email": "hola@gmail.com", "password": "pas123"})
     new_user = schemas.UserOut(**res.json())
-    print(res.json())
+    # print(res.json())
     assert new_user.email == "hola@gmail.com"
     assert res.status_code == 201
 
@@ -43,3 +32,20 @@ def test_login_user(client, test_user):
     assert id == test_user["id"]
     assert login_res.token_type == "bearer"
     assert res.status_code == 200
+
+
+@pytest.mark.parametrize(
+    "email, password, status_code",
+    [
+        ("wrongemail@gmail.com", "pas123", 403),
+        ("hola@gmail.com", "wrognpass", 403),
+        ("wrongemail@gmail.com", "wrongpass", 403),
+        (None, "pas123", 422),
+        ("hola@gmail.com", None, 422),
+    ],
+)
+def test_incorrect_login(test_user, client, email, password, status_code):
+    res = client.post("/login", data={"username": email, "password": password})
+
+    assert res.status_code == status_code
+    # assert res.json().get("detail") == "Invalid Credentials"
